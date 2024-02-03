1cc574d2b1e947f348bf26702afba5994dbde70f
JamesJJT <jastevens2222@hotmail.co.uk>
2024-02-03 12:30:42 +0000

    Added more functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import {useState, useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
 function App (){
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchMovies = async (title) => {
+        const response = await fetch(process.env.REACT_APP_MOVIE_BASE_URL + `&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search)
+    }
+    useEffect(() => {
+        searchMovies(searchTerm).then();
+    },[]);
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="app">
+        <h1>Movie Land</h1>
+
+        <div className="search">
+            <input
+                type="text"
+                placeholder="Search for a movie"
+                value={searchTerm}
+                onChange={(e) => {setSearchTerm(e.target.value)}}
+            />
+            <img src={SearchIcon}
+                 alt="Search"
+                 onClick={(e) => {searchMovies(searchTerm)}}
+            />
+        </div>
+        <div className="container">
+            {movies?.length === 0 && <h2>Search for a movie above</h2>}
+            {movies?.length > 0 && movies.map((movie) => (
+                <MovieCard
+                    movie={movie}
+                    key={movie.imdbID}
+                />
+            ))}
+        </div>
     </div>
   )};
 
